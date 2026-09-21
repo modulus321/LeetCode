f5fd24ac886a29928087a7f23c06b9cf3e572fbf
modulus321 <adityareddy3029@gmail.com>
2026-09-21 12:08:48 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.72%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.c
@@ -0,0 +1,11 @@
+struct ListNode* swapPairs(struct ListNode* head) {
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    struct ListNode* second = head->next;
+
+    head->next = swapPairs(second->next);
+    second->next = head;
+
+    return second;
+}
