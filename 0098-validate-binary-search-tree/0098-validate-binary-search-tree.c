18f3f1843980abb2a1cc0af77425a3ad92395f5f
modulus321 <adityareddy3029@gmail.com>
2026-09-21 12:24:33 +0530

    Time: 0 ms (100%), Space: 12.9 MB (44.36%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <limits.h>
+
+bool check(struct TreeNode* root, long min, long max) {
+
+    if (root == NULL)
+        return true;
+
+    if (root->val <= min || root->val >= max)
+        return false;
+
+    return check(root->left, min, root->val) &&
+           check(root->right, root->val, max);
+}
+
+bool isValidBST(struct TreeNode* root) {
+    return check(root, LONG_MIN, LONG_MAX);
+}
